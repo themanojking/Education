fdcad36d0692cacb8fe125e8692b6c1a2d5617b6
themanojking <kmanoj71737@gmail.com>
2025-07-25 21:27:26 +0530

    mentors all are set

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import { FaGraduationCap } from "react-icons/fa";
-import img1 from "../assets/home1.svg";
-import img2 from "../assets/home2.svg";
 import { IoIosSearch } from "react-icons/io";
+import { mentors, person } from "../Data";
 
 function Home() {
   return (
@@ -28,7 +27,10 @@ function Home() {
             </div>
             <div className="flex md:flex-row justify-center gap-5 md:gap-0 lg:gap-x-0.5 mt-1 md:mt-8 text-gray-500 font-semibold">
               <div>
-                <img src={img1} className="hidden lg:block blur-[2px] " />
+                <img
+                  src="/assets/home1.svg"
+                  className="hidden lg:block blur-[2px] "
+                />
               </div>
               <div className="mt-3 md:mt-0">
                 <h3 className="text-lg md:max-w-[75%]">
@@ -46,8 +48,60 @@ function Home() {
                 </div>
               </div>
               <div>
-                <img src={img2} className="hidden lg:block blur-[2px] " />
+                <img
+                  src="/assets/home2.svg"
+                  className="hidden lg:block blur-[2px] "
+                />
               </div>
+            </div>
+            <div className="flex justify-center gap-2 p-3 md:p-8 lg:gap-10 mt-3 md:mt-10">
+              {person.map((per) => (
+                <div
+                  key={per.id}
+                  className="rounded-full"
+                  style={{ backgroundColor: per.colors }}
+                >
+                  <img src={per.img} className=""></img>
+                </div>
+              ))}
+            </div>
+            <div className="mt-5">
+              <h1 className="text-2xl md:text-5xl font-bold">
+                Meet the Next Generation of{" "}
+              </h1>
+              <h1 className="text-2xl md:text-5xl font-bold">
+                Masterful Mentors
+              </h1>
+            </div>
+            <div className="mt-3 md:mt-5 -space-y-1">
+              <h3 className="text-lg text-gray-500 md:max-w-[75%] ">
+                Connect with the next wave of expert mentors, guiding you with
+                fresh perspectives, innovative insights, and proven expertise.
+              </h3>
+            </div>
+            <div className="flex flex-wrap justify-center items-center gap-5 lg:gap-10 mt-10 p-2 ">
+              {mentors.map((men) => (
+                <div className="border-2 border-gray-400 rounded-2xl  p-4 md:w-[45%] lg:w-[30%]">
+                  <img
+                    style={{ backgroundColor: men.colors }}
+                    className="w-[30%] rounded-full "
+                    src={men.img}
+                  ></img>
+                  <h1 className="text-xl font-bold mt-2">{men.name}</h1>
+                  <h3 className="text-lg font-semibold">{men.role}</h3>
+                  <div className="flex justify-center gap-4 mt-5">
+                    <button className="px-5 py-2 border-2 rounded-full text-sm lg:text-lg ">
+                      {men.caption1}
+                    </button>
+                    <button className="px-5 py-2 border-2 rounded-full text-sm lg:text-lg ">
+                      {men.caption2}
+                    </button>
+                    <button className="px-5 py-2 border-2 rounded-full text-sm lg:text-lg ">
+                      {men.caption3}
+                    </button>
+                  </div>
+                </div>
+              ))}
             </div>
           </center>
         </div>
